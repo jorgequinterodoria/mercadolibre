932a78f860b919b0432b6d6914668b09479bbb02
Jorge Quintero <jorgequintero@MacBook-Pro-de-Jorge.local>
2020-09-30 07:32:24 -0500

    first commit

---
+++ build/precache-manifest.474ba54cb6af220e1594dd9ddeb3ed7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b970c7397e34d734515cfb7815e5bbad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1222721b4ec2dc84322f",
+    "url": "/static/css/main.d929930a.chunk.css"
+  },
+  {
+    "revision": "9a11226bdd04bdfdcc56",
+    "url": "/static/js/2.57108e1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57108e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1222721b4ec2dc84322f",
+    "url": "/static/js/main.57b424d7.chunk.js"
+  },
+  {
+    "revision": "9c9881a502ad73cbcc74",
+    "url": "/static/js/runtime-main.ceb19457.js"
+  }
+]);
